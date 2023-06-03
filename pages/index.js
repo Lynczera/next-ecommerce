14c98ec5b08cdee4032c7fb9afd412518fdbd569
Lynczera <leonardochen.code@gmail.com>
2023-06-03 00:30:45 -0600

    added new pages and fixed styles

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,23 @@
-import Image from 'next/image'
-import { useSession, signIn, signOut } from "next-auth/react"
-import { Inter } from 'next/font/google'
-import Nav from '@/components/nav';
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react"
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const { data: session } = useSession();
-  console.log(session);
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg text-black">Login with Google</button>
-        </div>
-      </div>
-    );
-  }
 
-  return (
-    <div className='bg-blue-900 min-h-screen '>
-      <Nav/>
-      <div> Signed in as {session.user.email} </div>
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Hello, {session?.user?.name}
+
+      </h2>
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <img src={session?.user?.image}  className="w-6 h-6"/>
+        <span className="">
+        {session?.user?.name}
+        </span>
+
+      </div>
     </div>
-  );
+  </Layout>
 }
